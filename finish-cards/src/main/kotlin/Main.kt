8f13a09b6c2136c2e251b9e16f1d13ee1af8232e
Aydar <Mr.ivaoimop@mail.ru>
2021-10-28 13:03:50 +0300

    some changes

--- finish-cards/src/main/kotlin/Main.kt
+++ finish-cards/src/main/kotlin/Main.kt
@@ -1,6 +1,2 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
 }
